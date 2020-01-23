347a4cc8ad43aa87f52928f96c69e13cb3ad4420
XZ-XuZhang <zhangxu852654@gmail.com>
2020-01-23 18:56:11 +0000

    fixed some crawler problem

--- crawler.py
+++ crawler.py
@@ -1,32 +1,50 @@
 from recipe_scrapers import scrape_me
 from usp.tree import sitemap_tree_for_homepage
 import json
+import signal
+import time
 
 
+
+class TimeOutException(Exception):
+   pass
+
+def alarm_handler(signum, frame):
+    print("ALARM signal received")
+    raise TimeOutException()
+
+docID=0
 urllist=[]
 recipes_dict={}
 
-tree = sitemap_tree_for_homepage('https://www.bbcgoodfood.com/')
+tree = sitemap_tree_for_homepage("https://www.bbc.co.uk/food/")
 for page in tree.all_pages():
     url=page.url
-    if "https://www.bbcgoodfood.com/recipes/" in url:
+    if "https://www.bbc.co.uk/food/recipes/" in url:
         urllist.append(page.url)
+print(urllist)
+
+signal.signal(signal.SIGALRM, alarm_handler)
+
+with open('data.txt','w') as fp:
+    for url in urllist:
+        signal.alarm(8)
+
+        print(url)
+        scraper = scrape_me(url)
+        title=scraper.title()
+        time=scraper.total_time()
+        ingredients=scraper.ingredients()
+        docID +=1
+        fp.write(str(docID) + "\n")
+        fp.write(title + "\n")
+        ingredients_str = '   '.join(ingredients)
+        fp.write(ingredients_str + "\n")
+        fp.write("\n")
+        print("success")
 
 
+signal.alarm(0)
 
-for url in [urllist[0]]:
-    print(url)
-    scraper = scrape_me(url)
-    title=scraper.title()
-    time=scraper.total_time()
-    ingredients=scraper.ingredients()
-    yields=scraper.yields()
-    recipes_dict[title]={}
-    recipes_dict[title]["time"]=time
-    recipes_dict[title]["ingredients"]=ingredients
-    recipes_dict[title]["yields"]=yields
-
-print(recipes_dict)
-
-with open('data.json','w') as fp:
-    json.dump(recipes_dict,fp)
+# with open('data.json','w') as fp:
+#     json.dump(recipes_dict,fp)
