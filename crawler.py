0541c14155d99474a3e5dcffe4ad49372b0e7386
XZ-XuZhang <zhangxu852654@gmail.com>
2020-01-28 16:59:09 +0000

    fixed bug

--- crawler.py
+++ crawler.py
@@ -22,29 +22,34 @@ for page in tree.all_pages():
     url=page.url
     if "https://www.bbc.co.uk/food/recipes/" in url:
         urllist.append(page.url)
-print(urllist)
+
+with open('url.txt','w') as fp:
+    for url in urllist:
+        fp.write(url+"\n")
 
 signal.signal(signal.SIGALRM, alarm_handler)
 
 with open('data.txt','w') as fp:
     for url in urllist:
         signal.alarm(8)
-
-        print(url)
-        scraper = scrape_me(url)
-        title=scraper.title()
-        time=scraper.total_time()
-        ingredients=scraper.ingredients()
-        docID +=1
-        fp.write(str(docID) + "\n")
-        fp.write(title + "\n")
-        ingredients_str = '   '.join(ingredients)
-        fp.write(ingredients_str + "\n")
-        fp.write("\n")
-        print("success")
-
+        try:
+            print(url)
+            scraper = scrape_me(url)
+            title=scraper.title()
+            time=scraper.total_time()
+            ingredients=scraper.ingredients()
+            if title and time and ingredients:
+                docID +=1
+                fp.write(str(docID) + "\n")
+                fp.write(str(url) + "\n")
+                fp.write(title + "\n")
+                ingredients_str = '   '.join(ingredients)
+                fp.write(ingredients_str + "\n")
+                fp.write("\n")
+                print("success")
+            else:
+                raise Exception
+        except:
+            pass
 
 signal.alarm(0)
-
-# with open('data.json','w') as fp:
-#     json.dump(recipes_dict,fp)
